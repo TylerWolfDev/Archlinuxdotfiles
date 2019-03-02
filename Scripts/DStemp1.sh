4ee0de39642c7624e64f5d5a35be170be92c4cc4
wang-chung <wangchung1226@163.com>
2019-03-02 10:16:48 +0800

    .

--- Scripts/DStemp1.sh
+++ Scripts/DStemp1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-		cd /home/chung/.themes/Arc-Dark-pixel-blue/gtk-2.0/assets
-		ls
-		files=()
-		for i in *; do
-			convert $i -fuzz 0% -fill 'rgb(37,129,223)' -opaque  'rgb(26,115,232))' /home/chung/temppics/$i
-		done
+	cd /home/chung/.themes/Arc-Dark-pixel-blue/gtk-2.0/assets
+	ls
+	files=()
+	for i in *; do
+		convert $i -fuzz 0% -fill 'rgb(37,129,223)' -opaque  'rgb(26,115,232))' /home/chung/temppics/$i
+	done
